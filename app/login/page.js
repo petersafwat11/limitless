5b476f2650e6911c6d05dd2ef77dda7c9bf0d7c0
peter safwat <petersafwat640@gmail.com>
2025-09-17 13:20:49 +0300

    enhanced the design of the login & forget & change pass

--- app/login/page.js
+++ app/login/page.js
@@ -16,31 +16,17 @@ const page = () => {
           <Image
             src="/svg/squares-2.svg"
             alt="squares"
-            width={1394}
-            height={706}
+            width={948}
+            height={480} // 1185×0.8, 600×0.8
             className={styles.squares}
           />
           <Image
             className={styles.image1}
-            src={"/svg/login-1.svg"}
+            src={"/svg/login-image.svg"}
             alt="login-1"
-            width={476}
-            height={531}
+            width={400}
+            height={410}
           />
-          <Image
-            className={styles.image2}
-            src={"/svg/login-2.svg"}
-            alt="login-1"
-            width={125}
-            height={128}
-          />
-          <Image
-            className={styles.image3}
-            src={"/svg/login-3.svg"}
-            alt="login-1"
-            width={154}
-            height={169}
-          />{" "}
           <Image
             className={styles.image4}
             src={"/svg/login-mobile.svg"}
