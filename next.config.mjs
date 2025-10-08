fc4b5fbac1b63977895c207c03d127094780f516
peter safwat <petersafwat640@gmail.com>
2025-10-08 03:29:09 +0300

    switch the backend link

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,8 @@
 const nextConfig = {
   env: {
     POST_CODE_ApiKey: process.env.POST_CODE_ApiKey,
-    NEXT_PUBLIC_API_URL:
-      // "https://api.limitlesscover.co.uk",
-      "http://localhost:8000",
+    NEXT_PUBLIC_API_URL: "https://api.limitlesscover.co.uk",
+    // "http://localhost:8000",
   },
 };
 
