3244282d10aa8470bca766c85d2908e1e212e43f
peter safwat <petersafwat640@gmail.com>
2025-11-24 16:57:19 +0200

    testing

--- app/test/page.js
+++ app/test/page.js
@@ -1,7 +1,6 @@
 import PolicyUpdated from "@/ui/emails/policyUpdated/PolicyUpdated";
 import ResetPassword from "@/ui/emails/resetPassword/ResetPassword";
 import PolicyConfirmation from "@/ui/emails/policyConfirmation/PolicyConfirmation";
-import GetInsuredToday from "@/ui/emails/getInsuredToday/GetInsuredToday";
 import React from "react";
 
 const page = () => {
@@ -9,8 +8,7 @@ const page = () => {
     <div>
       {/* <PolicyUpdated />
       <ResetPassword /> */}
-      {/* <PolicyConfirmation /> */}
-      <GetInsuredToday />
+      <PolicyConfirmation />
     </div>
   );
 };
